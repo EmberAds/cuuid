c5eb615f3f874fbf385c9d1e103a27209d6d81e2
Caius Durling <dev@caius.name>
2012-08-14 00:34:27 +0100

    Native java implementation

--- lib/cuuid.rb
+++ lib/cuuid.rb
@@ -2,8 +2,14 @@ require File.expand_path("cuuid_generator", File.dirname(__FILE__))
 
 module CUUID
 
+  if RUBY_PLATFORM["java"]
+    require "jruby"
+    import 'com.emberads.cuuid'
+  end
+
   # backwards compatible (& lazy)
   def self.generate
     Generator.generate
   end
+
 end
